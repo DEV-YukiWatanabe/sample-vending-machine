a2b599a46dae93a2b277b387cbde7bb9a98648c4
DEV-YukiWatanabe <yu_watanabe@codmon.co.jp>
2023-03-08 10:00:28 +0900

    feat: おつり計算クラスを途中まで

--- src/Domain/ChangeCalculator.php
+++ src/Domain/ChangeCalculator.php
@@ -4,9 +4,20 @@ namespace src\Domain;
 
 class ChangeCalculator
 {
-    private array $coin = [500, 100, 50, 10, 1];
+    private const COIN = [500, 100, 50, 10, 1];
 
     public function calculate(array $coins, int $price) :string {
+        $amount = $this->calculateTotalAmount($coins);
+        $change = $amount - $price;
         return '100 3 50 1 10 3';
+    }
+
+    private function calculateTotalAmount(array $coins) :int {
+        $total = 0;
+        foreach ($this::COIN as $value) {
+            $count = $coins[$value];
+            $total += $value * $count;
+        }
+        return $total;
     }
 }
